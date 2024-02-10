0125aabbf8676d3bc08f1efbf25970e87928c91e
RagendhuRamesh <ragendhuram@gmail.com>
2024-02-10 17:45:44 +0530

    Add project files.

---
+++ MarksUpdate/Models/RainbowSchoolDBContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MarksUpdate.Models
+{
+    public partial class RainbowSchoolDBContext : DbContext
+    {
+        public RainbowSchoolDBContext()
+        {
+        }
+
+        public RainbowSchoolDBContext(DbContextOptions<RainbowSchoolDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Class> Classes { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+        public virtual DbSet<Subject> Subjects { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=LUCIAN-12;database=RainbowSchoolDB;trusted_connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Class>(entity =>
+            {
+                entity.Property(e => e.ClassId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ClassID");
+
+                entity.Property(e => e.ClassName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("Student");
+
+                entity.HasIndex(e => e.ClassId, "idx_Student_ClassID");
+
+                entity.Property(e => e.StudentId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("StudentID");
+
+                entity.Property(e => e.ClassId).HasColumnName("ClassID");
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Class)
+                    .WithMany(p => p.Students)
+                    .HasForeignKey(d => d.ClassId)
+                    .HasConstraintName("FK__Student__ClassID__534D60F1");
+            });
+
+            modelBuilder.Entity<Subject>(entity =>
+            {
+                entity.HasIndex(e => e.SubjectName, "idx_Subjects_SubjectName");
+
+                entity.Property(e => e.SubjectId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("SubjectID");
+
+                entity.Property(e => e.SubjectName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ MarksUpdate/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarksUpdate.Models
+{
+    public partial class Student
+    {
+        public int StudentId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? ClassId { get; set; }
+        public int? Marks { get; set; }
+
+      public virtual Class? Class { get; set; }
+    }
+}
